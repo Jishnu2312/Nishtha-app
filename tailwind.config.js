ab392fab46b5259d1324684275aff250cba12ec5
Jishnu2312 <85932059+Jishnu2312@users.noreply.github.com>
2025-07-13 13:03:06 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'electric-blue': '#3b82f6',
+      },
+    },
+  },
+  plugins: [],
+};
